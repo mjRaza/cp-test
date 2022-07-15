5c48eaf29aba76b0828ef1dcf91896a143114b11
mjRaza <jaffarrazadar@gmail.com>
2022-07-15 12:07:51 -0700

    done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes,Route } from 'react-router-dom';
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Form from "./pages/Form/Form";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes >
+      <Route  path="/" element={<Form />} />
+      <Route path="home" element={<Home  />} />
+      <Route path="add-post" element={<CreatePost  />} />
+    </Routes>
   );
 }
 
